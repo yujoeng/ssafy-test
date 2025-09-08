58f5643e75ad6b7fcfd6f2093da5905899ea6e5a
yujoeng <dnwjd812@gmail.com>
2025-09-09 08:16:14 +0900

    0909

--- IM대비문제/6190_정곤이의단조증가하는수.py
+++ IM대비문제/6190_정곤이의단조증가하는수.py
@@ -18,13 +18,13 @@
 만약 Ai x Aj중에서 단조 증가하는 수가 없다면 -1을 출력한다.
 '''
 
-T= int(input())
-for tc in range(1, T+1):
-    N = int(input())
-    for i in range(N-1):
-        for j in range(j+1,N):
-            num = A[i] * A[j]
-            # 단조증가 
+# T= int(input())
+# for tc in range(1, T+1):
+#     N = int(input())
+#     for i in range(N-1):
+#         for j in range(j+1,N):
+#             num = A[i] * A[j]
+#             # 단조증가 
 
-            if 단조증가 
+#             if 단조증가 
             
